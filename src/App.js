e6853db19c6423d26883a648a92ea8f6d769fed1
yahya <yahya.ensas@hotmail.com>
2020-12-14 22:19:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+class App extends Component{
+  render(){
+    const name = 'John Doe';
+    const loading = true;
+   
+    return (
+      <div className="App">
+        {loading ? <h4>Loading ...</h4> : <h1>Hello {name}</h1>}
+        
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
